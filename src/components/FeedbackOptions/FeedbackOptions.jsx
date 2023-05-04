17621dd298675e1bf773f4f44c808d86ed3faaf0
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-05-05 00:44:27 +0200

    changes

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './feedbackOptions.module.css';
+
+class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+    return (
+      <div className={styles['feedback-options']}>
+        {Object.keys(options).map(option => (
+          <button
+            className={styles['button-grey']}
+            key={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
